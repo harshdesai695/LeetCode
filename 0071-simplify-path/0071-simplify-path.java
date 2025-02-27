00afc59a1f46269dfec8e6fe45fca9ed337cd0c0
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2025-02-27 20:22:28 +0530

    Time: 9 ms (14.59%), Space: 43.9 MB (25.98%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> st = new Stack<>();
+        String[] ch = (path.split("/"));
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i].equals("..")) {
+                if (!st.isEmpty()) {
+                    st.pop();
+                }
+            } else if (ch[i].equals("")) {
+                continue;
+            } else if (!ch[i].isEmpty() && !ch[i].equals(".")) {
+                st.push(ch[i]);
+            }
+        }
+        System.out.print(st);
+        StringBuilder result = new StringBuilder();
+        while (!st.isEmpty()) {
+            result.insert(0, "/" + st.pop());
+        }
+        return result.length() > 0 ? result.toString() : "/";
+    }
+}
