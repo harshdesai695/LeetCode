cfe0dd31adce994079639b3e034ef012190e5939
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2025-04-26 11:29:53 +0530

    Time: 20 ms (16.12%), Space: 49.3 MB (10.28%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,16 +1,32 @@
-public class Solution {
+class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> res = new HashMap<>();
-        for (String s : strs) {
-            int[] count = new int[26];
-            for (char c : s.toCharArray()) {
-                count[c - 'a']++;
+        List<List<String>> ans = new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String str:strs){
+            int[] indexArr = stringToIndexArray(str);
+            String key = Arrays.toString(indexArr);
+            if(map.containsKey(key)){
+                map.get(key).add(str);
+            }else{
+                List<String> newList = new ArrayList<>();
+                newList.add(str);
+                map.put(key,newList);
             }
-            String key = Arrays.toString(count);
-            // System.out.print("Key:"+count);
-            res.putIfAbsent(key, new ArrayList<>());
-            res.get(key).add(s);
         }
-        return new ArrayList<>(res.values());
+        ans.addAll(map.values());
+        return ans;
+    }
+
+    public static int[] stringToIndexArray(String str) {
+        if (str == null) {
+            return null;
+        }
+        int[] indexArray = new int[26];
+        for (char ch : str.toCharArray()) { 
+            if (ch >= 'a' && ch <= 'z') {
+                indexArray[ch - 'a']++; 
+            }
+        }
+        return indexArray;
     }
 }
