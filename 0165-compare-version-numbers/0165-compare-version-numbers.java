e1d6657999b7cf400c4b6037a5bf79930695ceab
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2025-04-30 20:04:49 +0530

    Time: 1 ms (73.94%), Space: 41.8 MB (30.92%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int maxLength = Math.max(v1.length, v2.length);
+        v1 = Arrays.copyOf(v1, maxLength);
+        v2 = Arrays.copyOf(v2, maxLength);
+        for (int i = 0; i < maxLength; i++) {
+            if (v1[i] == null)
+                v1[i] = "0";
+            if (v2[i] == null)
+                v2[i] = "0";
+
+            int num1 = Integer.parseInt(v1[i]);
+            int num2 = Integer.parseInt(v2[i]);
+
+            if (num1 < num2)
+                return -1;
+            if (num1 > num2)
+                return 1;
+        }
+        // for (int i = 0; i < maxLength; i++) {
+        //     if (Integer.parseInt(v1[i]) < Integer.parseInt(v2[i])) {
+        //         version = -1;
+        //     } else if (Integer.parseInt(v1[i]) > Integer.parseInt(v2[i])) {
+        //         version = 1;
+        //     }
+        // }
+        return 0;
+    }
+}
