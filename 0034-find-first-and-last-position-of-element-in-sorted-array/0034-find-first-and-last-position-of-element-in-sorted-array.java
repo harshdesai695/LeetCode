a2e6fc5b11a58d07c366113a981b1a870d67bbbb
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2025-06-28 13:32:20 +0530

    Time: 0 ms (100%), Space: 46.3 MB (8.55%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,32 +1,37 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = { -1, -1 };
-        int low = 0;
-        int high = nums.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        int[] ans = { -1, -1 };
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                result[0] = mid;
-                high = mid - 1;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
+                ans[0] = mid;
+                right = mid - 1;
+            }
+            else if (nums[mid] > target) {
+                right = mid - 1;
             } else {
-                low = mid + 1;
+                left = mid + 1;
             }
         }
-        low=0;
-        high=nums.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        left=0;
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                result[1] = mid;
-                low = mid + 1;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
+                ans[1] = mid;
+                left = mid + 1;
+            }
+            else if (nums[mid] > target) {
+                right = mid - 1;
             } else {
-                low = mid + 1;
+                left = mid + 1;
             }
         }
-        return result;
+
+        
+        return ans;
     }
 }
