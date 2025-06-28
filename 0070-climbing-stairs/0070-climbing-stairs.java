89d6cb35da675b034c6cc5b955971d71cfa36c17
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2025-06-28 13:02:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,18 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
+        int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
-
-       return climbStair(n,dp);
+        return countWays(n, dp);
     }
 
-    public int climbStair(int n, int[] dp) {
+    public int countWays(int n, int[] dp) {
         if (n == 0 || n == 1) {
             return 1;
         }
-        if (dp[n] == -1) {
-            dp[n] = climbStair(n - 1,dp) + climbStair(n - 2,dp);
+        if (dp[n] != -1) {
+            dp[n] = countWays(n - 1, dp) + countWays(n - 2, dp);
         }
+
         return dp[n];
     }
 }
