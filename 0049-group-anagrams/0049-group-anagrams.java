9326b6cd93c408fec6b994e6d1cf2e556d4f5ddd
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2025-06-29 15:08:10 +0530

    Time: 18 ms (24.56%), Space: 49.7 MB (5.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,29 +2,30 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> ans = new ArrayList<>();
         HashMap<String, List<String>> map = new HashMap<>();
-        for(String str:strs){
-            int[] indexArr = stringToIndexArray(str);
-            String key = Arrays.toString(indexArr);
-            if(map.containsKey(key)){
+        for (String str : strs) {
+            int[] stringArray = stringToIndex(str);
+            String key = Arrays.toString(stringArray);
+            if (map.containsKey(key)) {
                 map.get(key).add(str);
-            }else{
-                List<String> newList = new ArrayList<>();
-                newList.add(str);
-                map.put(key,newList);
+            } else {
+                List<String> temp = new ArrayList<>();
+                temp.add(str);
+                map.put(key, temp);
             }
         }
         ans.addAll(map.values());
         return ans;
     }
 
-    public static int[] stringToIndexArray(String str) {
+    public int[] stringToIndex(String str) {
+
         if (str == null) {
             return null;
         }
         int[] indexArray = new int[26];
-        for (char ch : str.toCharArray()) { 
+        for (char ch : str.toCharArray()) {
             if (ch >= 'a' && ch <= 'z') {
-                indexArray[ch - 'a']++; 
+                indexArray[ch - 'a']++;
             }
         }
         return indexArray;
