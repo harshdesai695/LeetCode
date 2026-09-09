4c6d10e79436a99a6ad4bc8dfc18fc6332b1aa03
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2026-09-09 18:45:20 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,25 @@
 class Solution {
+
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return countSteps(dp, n);
+        int[] m = new int[n + 1];
+        Arrays.fill(m, -1);
+        return dp(n, m);
     }
 
-    public int countSteps(int[] dp, int n) {
-        if (n == 1 || n == 0) {
-            return 1;
+    public int dp(int n, int[] m) {
+        if (n <= 0) {
+            return 0;
         }
-        if (dp[n] == -1) {
-            dp[n] = countSteps(dp, n - 1) + countSteps(dp, n - 2);
+        if (n <= 2) {
+            return n;
         }
-        return dp[n];
+        if (m[n] != -1) {
+            return m[n];
+        }
+        return m[n] = dp(n - 2, m) + dp(n - 1, m);
     }
 }
+
+// 5 
+// 1 (5-1) = 4 
+// 2 (5-2) = 3 
