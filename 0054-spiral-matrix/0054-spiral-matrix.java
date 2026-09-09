aff64face63d2d0a276f79a60b1bdf3fe3a8d98f
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2026-09-09 20:11:05 +0530

    Time: 0 ms (100%), Space: 43.4 MB (9.52%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,32 +1,67 @@
 class Solution {
+
+    List<Integer> ans = new ArrayList<>();
+    int n = 0;
+    int m = 0;
+
     public List<Integer> spiralOrder(int[][] matrix) {
-        ArrayList<Integer> ans = new ArrayList<>();
-		int n = matrix.length; // Row
-		int m = matrix[0].length;// Column
-		int r = m - 1, b = n - 1;
-		int l = 0, t = 0;
-		while (t <= b && l <= r) {
-            for (int i = l; i <= r; i++) {
-				ans.add(matrix[t][i]);
-			}
-			t++;
-			for (int i = t; i <= b; i++) {
-				ans.add(matrix[i][r]);
-			}
-			r--;
-			if (t <= b) {
-				for (int i = r; i >= l; i--) {
-					ans.add(matrix[b][i]);
-				}
-				b--;
-			}
-			if (l <= r) {
-				for (int i = b; i >= t; i--) {
-					ans.add(matrix[i][l]);
-				}
-				l++;
-			}
-		}
+        n = matrix.length;
+        m = matrix[0].length;
+        int totalElement = n * m;
+        int startRow = 0;
+        int startColumn = 0;
+        int endRow = n - 1;
+        int endColumn = m - 1;
+        while (totalElement != ans.size()) {
+            leftToRight(startColumn, endColumn, startRow, matrix);
+            startRow++;
+            if (totalElement == ans.size()) {
+                break;
+            }
+            topToBottom(startRow, endRow, endColumn, matrix);
+            endColumn--;
+            if (totalElement == ans.size()) {
+                break;
+            }
+            rightToLeft(startColumn, endColumn, endRow, matrix);
+            endRow--;
+            if (totalElement == ans.size()) {
+                break;
+            }
+            bottomToTop(startRow, endRow, startColumn, matrix);
+            startColumn++;
+            if (totalElement == ans.size()) {
+                break;
+            }
+            // System.out.println(ans);
+        }
+
         return ans;
+
     }
+
+    public void leftToRight(int start, int end, int row, int[][] matrix) {
+        for (int i = start; i <= end; i++) {
+            ans.add(matrix[row][i]);
+        }
+    }
+
+    public void topToBottom(int start, int end, int column, int[][] matrix) {
+        for (int i = start; i <= end; i++) {
+            ans.add(matrix[i][column]);
+        }
+    }
+
+    public void rightToLeft(int start, int end, int row, int[][] matrix) {
+        for (int i = end; i >= start; i--) {
+            ans.add(matrix[row][i]);
+        }
+    }
+
+    public void bottomToTop(int start, int end, int column, int[][] matrix) {
+        for (int i = end; i >= start; i--) {
+            ans.add(matrix[i][column]);
+        }
+    }
+
 }
