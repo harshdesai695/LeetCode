d9d173081a5ac9fc74bae74b6cf0fa4700d218e1
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2026-09-10 19:11:52 +0530

    Time: 3 ms (87.7%), Space: 52.6 MB (21.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,34 +1,43 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int numIslands=0;
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
+    int noOfIsland = 0;
 
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] == '1') {
-                    numIslands++; // Found a new island
-                    dfs(grid, r, c); // Explore and mark the entire island
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    noOfIsland++;
                 }
             }
         }
-        return numIslands;
+        return noOfIsland;
     }
 
-    private void dfs(char[][] grid, int r, int c) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0') {
+    public void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0' || grid[i][j] == 'v') {
             return;
         }
-
-        grid[r][c] = '0'; 
-
-        dfs(grid, r + 1, c); // down
-        dfs(grid, r - 1, c); // up
-        dfs(grid, r, c + 1); // right
-        dfs(grid, r, c - 1); // left
+        grid[i][j] = 'v';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
+
 }
+
+// [
+//   ["1","1","1","1","0"],
+//   ["1","1","0","1","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","0","0","0"]
+// ]
+
+// [
+//   ["1","1","0","0","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","1","0","0"],
+//   ["0","0","0","1","1"]
+// ]
