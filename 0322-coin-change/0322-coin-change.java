638032730eb8cd45d3f943b7e3a850d3e48f6c69
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2026-09-14 10:20:20 +0530

    Time: 15 ms (83.62%), Space: 46.8 MB (35.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,24 +1,30 @@
 class Solution {
+
+    // 1. Choice take or noTake
+    // 2. min no of choins
+    int[][] mt;
+
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] mt = new int[n + 1][amount + 1];
-        for (int i = 0; i <= n; i++) {
-            mt[i][0] = 0; 
+        mt = new int[n + 1][amount + 1];
+        for (int i = 0; i < n + 1; i++) {
+            mt[i][0] = 0;
         }
+
         for (int j = 1; j <= amount; j++) {
-            mt[0][j] = Integer.MAX_VALUE - 1; 
+            mt[0][j] = amount + 1;
         }
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= amount; j++) {
                 if (coins[i - 1] <= j) {
-                    mt[i][j] = Math.min(mt[i - 1][j], 1 + mt[i][j - coins[i - 1]]);
+                    mt[i][j] = Math.min((1 + mt[i][j - coins[i - 1]]), mt[i - 1][j]);
                 } else {
                     mt[i][j] = mt[i - 1][j];
                 }
             }
         }
-
-        return mt[n][amount] == Integer.MAX_VALUE - 1 ? -1 : mt[n][amount];
+        return mt[n][amount] == (amount+1) ? -1 : mt[n][amount];
     }
+
 }
