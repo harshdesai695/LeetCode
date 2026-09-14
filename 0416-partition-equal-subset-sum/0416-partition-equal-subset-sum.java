2a74f05ba74d60d0508bb5fc261484af092a9477
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2026-09-14 12:57:56 +0530

    Time: 56 ms (41.71%), Space: 47.2 MB (63.76%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,23 +1,17 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
-        long sum = 0;
+    boolean[][] mt;
 
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
         for (int i : nums) {
-            sum += i;
+            sum = sum + i;
         }
         if (sum % 2 != 0) {
             return false;
-        } else {
-            return SubSet(nums, (int) sum / 2);
         }
-
-    }
-
-    public static boolean SubSet(int[] arr, int sum) {
-        int n = arr.length;
-        boolean[][] mt = new boolean[n + 1][sum + 1];
-
-        // Step-1 Init Matrix
+        sum = sum / 2;
+        mt = new boolean[n + 1][sum + 1];
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= sum; j++) {
                 if (j == 0) {
@@ -28,13 +22,13 @@ class Solution {
             }
         }
 
-        // Step-2 Iterative Method
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= sum; j++) {
-                if (arr[i - 1] <= j) {
-                    mt[i][j] = mt[i - 1][j] || mt[i - 1][j - arr[i - 1]];
+                if (nums[i - 1] <= j) {
+                    mt[i][j] = mt[i-1][j-nums[i-1]] || mt[i-1][j];
+                    // (nums, sum - nums[i - 1], i - 1) || (nums, sum, i - 1);
                 } else {
-                    mt[i][j] = mt[i - 1][j];
+                    mt[i][j] = mt[i-1][j];
                 }
             }
         }
@@ -42,4 +36,20 @@ class Solution {
         return mt[n][sum];
     }
 
+    // public boolean partition(int[] nums, int target, int n) {
+    //     if (n == 0) {
+    //         return false;
+    //     }
+    //     if (target == 0) {
+    //         return true;
+    //     }
+    //     if (mt[n][target] != -1) {
+    //         return mt[n][target];
+    //     }
+    //     if (nums[n - 1] <= target) {
+    //         return mt[n][target] = partition(nums, target - nums[n - 1], n - 1) || partition(nums, target, n - 1);
+    //     } else {
+    //         return mt[n][target] = partition(nums, target, n - 1);
+    //     }
+    // }
 }
